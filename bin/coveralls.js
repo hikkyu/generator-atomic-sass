c088cdab5b0203e0a7a691f6e51710a2ef0d9118
dberseron <damien.berseron@me.com>
2014-09-24 23:09:50 +0200

    add coverall

---
+++ bin/coveralls.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+var handleInput = require('../lib/handleInput');
+var logger = require('../lib/logger');
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var input = 'istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage';
+
+process.stdin.on('data', function(chunk) {
+    input += chunk;
+});
+
+process.stdin.on('end', function() {
+    handleInput(input, function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
