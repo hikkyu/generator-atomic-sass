532948f3ea38595b6faa12c73fedaa12b5a8f410
dberseron <damien.berseron@me.com>
2014-09-25 00:06:56 +0200

    covrall again and again

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcov -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
